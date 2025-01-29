18e0ad6b13b4bc2f96cec6b053f5675fed8070a3
naumgh <naum.hoffman@gmail.com>
2025-01-29 14:35:34 -0800

    commiting some more db fixes for sqlite

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'drizzle-kit';
+const DATABASE_URL = process.env.DATABASE_URL || "file:./drizzle.sqlite";
+
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 
 	dbCredentials: {
-		url: process.env.DATABASE_URL
+		url: DATABASE_URL
 	},
 
 	verbose: true,
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,12 @@
-import { drizzle } from 'drizzle-orm/libsql';
+import { drizzle } from 'drizzle-orm/better-sqlite3';
 import { createClient } from '@libsql/client';
+//@ts-ignore
 import { env } from '$env/dynamic/private';
+//@ts-ignore
+import Database from 'better-sqlite3';
 //if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = createClient({ url: env.DATABASE_URL });
-export const db = drizzle(client);
+//const client = createClient({ url: env.DATABASE_URL });
+//export const db = drizzle(client);
+
+const sqlite = new Database('mydb.sqlite');
+export const db = drizzle(sqlite);
