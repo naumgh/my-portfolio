717cefe823cdc665ca7f5fd10340297e349b8de8
naumgh <naum.hoffman@gmail.com>
2025-01-29 21:14:09 -0800

    restarting fresh

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'drizzle-kit';
-
-// Use `/tmp/drizzle.sqlite` in Vercel, otherwise use local file
-const DATABASE_URL =
-  process.env.DATABASE_URL || "file:./drizzle.sqlite";
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
-  schema: './src/lib/server/db/schema.ts',
-  dbCredentials: { url: DATABASE_URL },
-  verbose: true,
-  strict: true,
-  dialect: 'sqlite'
+	schema: './src/lib/server/db/schema.ts',
+
+	dbCredentials: {
+		url: process.env.DATABASE_URL
+	},
+
+	verbose: true,
+	strict: true,
+	dialect: 'sqlite'
 });
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,23 +1,6 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
-//@ts-ignore
-import Database from "better-sqlite3";
-import fs from "fs";
-import path from "path";
-
-// Use `/tmp/drizzle.sqlite` on Vercel, otherwise use local file
-const localDbPath = path.resolve("drizzle.sqlite");
-const vercelDbPath = "/tmp/drizzle.sqlite";
-const dbPath = process.env.VERCEL ? vercelDbPath : localDbPath;
-
-if (process.env.VERCEL) {
-  // Only copy if the local database exists
-  if (fs.existsSync(localDbPath)) {
-    console.log("Copying SQLite database to /tmp/");
-    fs.copyFileSync(localDbPath, vercelDbPath);
-  } else {
-    console.warn("⚠️ drizzle.sqlite does not exist. Skipping copy.");
-  }
-}
-
-const sqlite = new Database(dbPath);
-export const db = drizzle(sqlite);
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
+import { env } from '$env/dynamic/private';
+if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const client = createClient({ url: env.DATABASE_URL });
+export const db = drizzle(client);
