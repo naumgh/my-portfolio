47ecd4f6ee65c7fa65595f7e748a0dab7dbe0878
naumgh <naum.hoffman@gmail.com>
2025-01-31 12:15:06 -0800

    commiting python script again

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -16,7 +16,7 @@
 
     async function handleSubmit() {
         try {
-            const response = await fetch('/api/send-email', {
+            const response = await fetch('../../../api/send-email', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
