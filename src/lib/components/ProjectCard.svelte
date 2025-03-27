c9f365b7d441e19e43ed446c73c36250c8514c93
naumgh <naum.hoffman@gmail.com>
2025-03-26 19:30:22 -0700

    changing description and title based on reccomendations

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -42,7 +42,7 @@
         <div class="flex items-center justify-between p-2">
             <div class="flex flex-col w-full max-w-[calc(100%-5rem)]">
                 <h2 class="text-xs font-bold text-primary">{title}</h2>
-                <p class="text-[0.625rem] text-[#a1a1aa] mt-1 whitespace-pre-wrap break-words">{description}</p>
+                <p class="text-[0.7rem] text-[#a1a1aa] mt-1 whitespace-pre-wrap break-words">{description}</p>
             </div>
             <div class="flex items-center space-x-2">
                 <a
