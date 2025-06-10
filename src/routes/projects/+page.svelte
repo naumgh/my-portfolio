0804d7a76b35065b598ec4aef667575ef37567bf
naumgh <naum.hoffman@gmail.com>
2025-06-10 13:52:19 -0700

    commiting github profile pic

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -56,7 +56,7 @@
 
         <ProjectHeader
             title="FastTrack’D Projects"
-            iconSrc="https://www.vectorlogo.zone/logos/fasttrackd/fasttrackd-icon.svg"
+            iconSrc="https://cooksys.com/wp-content/uploads/2024/05/Cook-Systems-IT-Staffing-Solutions-Logo.svg"
             iconAlt="FastTrack'D Logo"
             titleColor="#ff6f61"
         />
