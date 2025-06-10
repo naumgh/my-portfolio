0e44c225206f3607e36165e9142d8fbf15aadfa2
naumgh <naum.hoffman@gmail.com>
2025-06-10 09:38:05 -0700

    commiting github profile pic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,13 +17,24 @@ name can now be set from ouside the component, by a parent component
     export let name = "Naum Hoffman";
     export let jobTitle = "Software Engineer / Data Analyst";
     import Navbar from "$lib/components/Navbar.svelte";
+
+fetch("https://api.github.com/users/naumgh")
+  .then((res) => res.json())
+  .then((data) => {
+    document.getElementById("github-profile-pic").src = data.avatar_url;
+    //document.getElementById("bio").textContent = data.bio;
+});
 </script>
 
 <Navbar />
 
 <main class="flex flex-col items-center justify-center min-h-screen bg-background text-primary font-sans text-center">
 
-    <img src="/me2.jpg" alt="Naum Hoffman" class="w-36 h-36 rounded-full border-2 border-accent shadow-lg mb-6">
+    <img
+    id="github-profile-pic"
+    class="w-36 h-36 rounded-full border-2 border-accent shadow-lg mb-6"
+    alt="GitHub Profile Picture"
+    />
 
     <h1 class="text-4xl sm:text-5xl md:text-6xl font-bold mb-2">{name}</h1>
     <p class="text-lg sm:text-xl text-secondary mb-6">{jobTitle}</p>
